e153309696fa10cf9537e9757ce0eb7bc427a029
Benji Chimex <benjichimex@MacBooks-MacBook-Pro.local>
2023-12-23 11:18:39 +0100

    another commit

--- __web3__/config.js
+++ __web3__/config.js
@@ -1,3 +1,3 @@
-export const IDBot_CA = "0xeb98902ABC1CaF87d60f50823fbbA7C8e60b39e0"
+export const IDBot_CA = "0x9FC58E3A8d633D25Aab74106b01a37d9E5ba90dd"
 
 export const Super_Owner = "0x6627f8ddc81057368F9717042E38E3DEcb68dAc3"
